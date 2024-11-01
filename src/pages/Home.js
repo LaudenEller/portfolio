0456053c8946032d63c27e3b8b646d3d31e4a3af
Lauden Eller <laudlauden@gmail.com>
2024-10-31 20:07:45 -0700

    Initial commit

---
+++ src/pages/Home.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Home = () => {
+    return (
+      <div className="home">
+        {/* Replace text with welcome message component */}
+        <h1>Welcome to My Portfolio</h1>
+        <p>This is a simple portfolio website showcasing my work.</p>
+      </div>
+    );
+  };
+  
+  export default Home;
