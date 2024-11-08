cb73894bf2598d14cd70e8d7f1a03098585df7c3
Lauden Eller <laudlauden@gmail.com>
2024-11-08 10:46:06 -0800

    Set up sticky header and adjusted the margins around nav elements. Still needs lots of css corrections

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -2,28 +2,39 @@ import React, { useContext } from 'react';
 import ThemeContext from '../../contexts/ThemeContext';
 import { Logo } from '../Logo/logo';
 import styles from './Navbar.module.css';
+import { Navbar as BootstrapNavbar, Nav, Container } from 'react-bootstrap';
 
 const Navbar = () => {
   const { toggleTheme, theme } = useContext(ThemeContext);
 
   return (
-    <nav className={styles.navbar}>
-      <div className={styles.logo}>
-        <Logo />
+    <BootstrapNavbar
+      className={`${styles.navbar} ${theme === 'dark' ? 'bg-dark' : 'bg-light'}`}
+      fixed="top"
+    >
+      <Container
+      className={`${styles.navContainer}`}>
+        {/* Logo */}
+        <BootstrapNavbar.Brand href="#">
+          <div className={styles.logo}>
+            <Logo />
+          </div>
+        </BootstrapNavbar.Brand>
+
+        {/* Navbar Links */}
+          <Nav className="me-auto">
+            <Nav.Link href="#about" className="nav-link">About</Nav.Link>
+            <Nav.Link href="#projects" className="nav-link">Projects</Nav.Link>
+            <Nav.Link href="#work" className="nav-link">Work</Nav.Link>
+            <Nav.Link href="#contact" className="nav-link">Contact</Nav.Link>
+          </Nav>
+
+        {/* Theme Toggle Button */}
+          <div className={styles.toggleContainer} onClick={toggleTheme}>
+        <div className={`${styles.themeToggle} ${theme === 'dark' ? styles.dark : styles.light}`}></div>
       </div>
-      <div className={styles.navLinks}>
-        <a href="#about" className="nav-link">About</a>
-        <a href="#projects" className="nav-link">Projects</a>
-        <a href="#work" className="nav-link">Work</a>
-        <a href="#contact" className="nav-link">Contact</a>
-      </div>
-      <button
-        onClick={toggleTheme}
-        className={`btn ${theme === 'dark' ? 'btn-light' : 'btn-dark'}`}
-      >
-        Toggle Theme
-      </button>
-    </nav>
+      </Container>
+    </BootstrapNavbar>
   );
 };
 
